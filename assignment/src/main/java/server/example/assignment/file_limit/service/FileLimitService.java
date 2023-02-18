7584e2dcd8392cfa1516738c1208db09b640dab2
Lee-DoHa <johnny550@naver.com>
2023-02-18 21:48:21 +0900

    Update FileLimitService.java

--- assignment/src/main/java/server/example/assignment/file_limit/service/FileLimitService.java
+++ assignment/src/main/java/server/example/assignment/file_limit/service/FileLimitService.java
@@ -1,7 +1,24 @@
 package server.example.assignment.file_limit.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import server.example.assignment.file_limit.domain.FileLimit;
+import server.example.assignment.file_limit.repository.FileLimitRepository;
+
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class FileLimitService {
+    private final FileLimitRepository fileLimitRepository;
+
+    public void create(String type) {
+        FileLimit createLimit = FileLimit.builder().type(type).build();
+        fileLimitRepository.save(createLimit);
+    }
+
+    public void delete(Long id) {
+        Optional<FileLimit> fileLimit = fileLimitRepository.findById(id);
+        fileLimit.ifPresent(fileLimitRepository::delete);
+    }
 }

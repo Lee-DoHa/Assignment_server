8e4f73d652bb6f6fea9584d9212f7a2e66d20672
Lee-DoHa <johnny550@naver.com>
2023-02-18 21:48:15 +0900

    Update FileLimitController.java

--- assignment/src/main/java/server/example/assignment/file_limit/controller/FileLimitController.java
+++ assignment/src/main/java/server/example/assignment/file_limit/controller/FileLimitController.java
@@ -1,7 +1,26 @@
 package server.example.assignment.file_limit.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import server.example.assignment.file_limit.service.FileLimitService;
 
 @RestController
+@RequiredArgsConstructor
 public class FileLimitController {
+
+    private final FileLimitService fileLimitService;
+
+    @PostMapping(value = "/fileLimit")
+    public void limit(@RequestBody String type) {
+        fileLimitService.create(type);
+    }
+
+    @DeleteMapping(value = "/fileLimit")
+    public void delete(@RequestBody Long id) {
+        fileLimitService.delete(id);
+    }
+
 }

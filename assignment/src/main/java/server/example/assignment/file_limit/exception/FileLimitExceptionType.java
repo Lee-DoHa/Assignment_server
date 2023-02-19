62d4d2cb4b2b32f47f16c43a3068940853601ad4
Lee-DoHa <johnny550@naver.com>
2023-02-19 13:11:56 +0900

    Update FileLimitExceptionType.java

--- assignment/src/main/java/server/example/assignment/file_limit/exception/FileLimitExceptionType.java
+++ assignment/src/main/java/server/example/assignment/file_limit/exception/FileLimitExceptionType.java
@@ -3,7 +3,9 @@ package server.example.assignment.file_limit.exception;
 import org.springframework.http.HttpStatus;
 
 public enum FileLimitExceptionType {
-    ALREADY_EXIST_NAME(600, HttpStatus.CONFLICT, "이미 존재하는 확장자입니다.");
+    ALREADY_EXIST_NAME(600, HttpStatus.CONFLICT, "이미 존재하는 확장자입니다."),
+    NAME_OVERLOAD(601, HttpStatus.BAD_REQUEST, "확장자 이름이 20자 초과되었습니다."),
+    LIMIT_OVERLOAD(602, HttpStatus.BAD_REQUEST, "확장자가 너무 많습니다.");
 
     private int errorCode;
     private HttpStatus httpStatus;

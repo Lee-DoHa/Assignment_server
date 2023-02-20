cf8aca7708d555974c89b6d640bbcdb62a697631
Lee-DoHa <johnny550@naver.com>
2023-02-20 14:31:47 +0900

    Update FileLimitService.java

--- assignment/src/main/java/server/example/assignment/file_limit/service/FileLimitService.java
+++ assignment/src/main/java/server/example/assignment/file_limit/service/FileLimitService.java
@@ -1,15 +1,19 @@
 package server.example.assignment.file_limit.service;
 
+import antlr.MismatchedCharException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import server.example.assignment.file_limit.domain.FileLimit;
+import server.example.assignment.file_limit.domain.FileLimitResponse;
 import server.example.assignment.file_limit.domain.LimitType;
-import server.example.assignment.file_limit.exception.FileLimitException;
-import server.example.assignment.file_limit.exception.FileLimitExceptionType;
+import server.example.assignment.file_limit.exception.*;
 import server.example.assignment.file_limit.repository.FileLimitRepository;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,7 +23,13 @@ public class FileLimitService {
 
     public void create(String name) {
         if(fileLimitRepository.findByName(name).isPresent()) {
-            throw new FileLimitException(FileLimitExceptionType.ALREADY_EXIST_NAME);
+            throw new AlreadyExistName("name", "이미 제한된 확장자입니다.");
+        }
+        if(name.length() > 20) {
+            throw new NameOverLoad("name", "확장자 이름은 20자를 초과할 수 없습니다.");
+        }
+        if(fileLimitRepository.countCustom() == 200) {
+            throw new LimitOverload("count", "커스텀 확장자는 200개를 초과할 수 없습니다.");
         }
         FileLimit createLimit = FileLimit.builder()
                 .name(name)
@@ -29,7 +39,15 @@ public class FileLimitService {
     }
 
     public void delete(Long id) {
-        Optional<FileLimit> fileLimit = fileLimitRepository.findById(id);
-        fileLimit.ifPresent(fileLimitRepository::delete);
+        FileLimit fileLimit = fileLimitRepository.findById(id).orElseThrow(LimitNotFound::new);
+        fileLimitRepository.delete(fileLimit);
+    }
+
+    public List<FileLimitResponse> get() {
+        List<FileLimitResponse> result = fileLimitRepository.findAll().stream()
+                .map(FileLimitResponse::new)
+                .collect(Collectors.toList());
+
+        return result;
     }
 }

c13f8d234907a530fc7cde9df836168686decc8e
Lee-DoHa <johnny550@naver.com>
2023-02-20 14:31:46 +0900

    Update FileLimitRepository.java

--- assignment/src/main/java/server/example/assignment/file_limit/repository/FileLimitRepository.java
+++ assignment/src/main/java/server/example/assignment/file_limit/repository/FileLimitRepository.java
@@ -1,10 +1,14 @@
 package server.example.assignment.file_limit.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import server.example.assignment.file_limit.domain.FileLimit;
 
 import java.util.Optional;
 
 public interface FileLimitRepository extends JpaRepository<FileLimit, Long> {
     Optional<FileLimit> findByName(String name);
+
+    @Query(value = "SELECT COUNT(*) FROM file_limit WHERE limit_type = 'CUSTOM'", nativeQuery = true)
+    int countCustom();
 }
